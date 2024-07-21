9a80cd56537f96c4b23170f28dd78e902e340bfb
Vedang <vedangssjgod@gmail.com>
2024-07-22 01:57:40 +0530

    Data is added to backend (data routes configured)

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authroutes from './routes/auth.routes.js'
 import connectToMongoDb from './db/connectToMongoDb.js'
+import dataroutes from './routes/data.routes.js'
+
+
 const app=express()
 dotenv.config()
 
@@ -11,6 +14,7 @@ app.use(express.json())
 //     res.send("Yo")
 // })
 app.use("/",authroutes);
+app.use("/",dataroutes);
 app.listen(PORT,()=>{
     connectToMongoDb();
     console.log(`server started on ${PORT}`);
