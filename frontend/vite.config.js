cf5b85306c279a952f36a9b86897787e04bee8a9
Vedang <vedangssjgod@gmail.com>
2024-07-23 13:22:05 +0530

    Deployment ready

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:"http://localhost:5000/api/",
+        target:"https://internship-portal-um18.onrender.com/api",
       },
     }
   }
